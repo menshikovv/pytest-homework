f3a2b3fd918d6e83e82e37a4c0eb467813b59aae
menshikov <matveydrobot15@gmail.com>
2024-02-18 17:48:05 +0300

    Fourth commit

--- django_testing/tests/conftest.py
+++ django_testing/tests/conftest.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from .models import Course
 from .serializers import CourseSerializer
 
-class CoursesViewSetTests(TestCase):
+class CoursesApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
@@ -17,20 +17,18 @@ class CoursesViewSetTests(TestCase):
         response = self.client.get(reverse('course-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_create_course(self):
-        data = {'name': 'History'}
-        response = self.client.post(reverse('course-list'), data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Course.objects.count(), 2)
-
-    def test_update_course(self):
-        updated_name = 'Updated Math'
-        data = {'name': updated_name}
-        response = self.client.put(self.course_url, data, format='json')
+    def test_get_single_course(self):
+        response = self.client.get(self.course_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Course.objects.get(id=self.course.id).name, updated_name)
 
-    def test_delete_course(self):
-        response = self.client.delete(self.course_url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Course.objects.count(), 0)
+    def test_filter_courses_by_id(self):
+        filter_url = f"{reverse('course-list')}?id={self.course.id}"
+        response = self.client.get(filter_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_filter_courses_by_name(self):
+        filter_url = f"{reverse('course-list')}?name={self.course_data['name']}"
+        response = self.client.get(filter_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
